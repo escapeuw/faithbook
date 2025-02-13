c0bd0171e64151f5ea3ab79f0f2bae5ff6312393
escapeuw <ghkdehd78999@gmail.com>
2025-02-12 21:47:37 -0500

    gitignore update

--- faithbook/backend/config/database.js
+++ faithbook/backend/config/database.js
@@ -5,13 +5,19 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
     process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true, // Railway requires SSL
+            rejectUnauthorized: false,
+        },
+    },
     logging: false, // True if want to see SQL queries in the console
 }); // 
 
 
 sequelize.authenticate()
-    .then(() => console.log("PostgreSQL Connected"))
-    .catch((err) => console.error("PostgreSQL CONNECTION ERROR:", err));
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.error("DB CONNECTION ERROR:", err));
 
 
 module.exports = sequelize;
