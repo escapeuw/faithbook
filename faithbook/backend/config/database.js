394f8122328fd1f85808f8af74fa8e90a8bd393c
escapeuw <ghkdehd78999@gmail.com>
2025-02-12 22:08:44 -0500

    db url to railway

--- faithbook/backend/config/database.js
+++ faithbook/backend/config/database.js
@@ -1,9 +1,7 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config(); // Load environment variables
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     dialectOptions: {
         ssl: {
