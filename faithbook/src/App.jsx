632bf5cb82ece9692e925b43a05f2c27104c2b2f
Daniel Hwang <ghkdehd78999@gmail.com>
2025-02-15 16:31:02 -0500

    testing routes

--- faithbook/src/App.jsx
+++ faithbook/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/faithbook" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/home/*" element={<Home />} />
         </Routes>
