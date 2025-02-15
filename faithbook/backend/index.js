a80777a8139bf1d34fcd7ff4c1524d0d91fa9a4c
Daniel Hwang <ghkdehd78999@gmail.com>
2025-02-15 06:30:24 -0500

    Update index.js

--- faithbook/backend/index.js
+++ faithbook/backend/index.js
@@ -5,17 +5,9 @@ const authRoutes = require("./routes/auth");
 require("dotenv").config();
 
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.json());
 app.use(cors());
 
-// ✅ Handle React client-side routing (SPA behavior)
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-  
-  // Start the server
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // routes
 app.use("/api/auth", authRoutes);
