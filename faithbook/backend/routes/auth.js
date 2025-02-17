f83394fc250ada95a848376b986f91fb1257391f
Daniel Hwang <ghkdehd78999@gmail.com>
2025-02-17 03:54:14 -0500

    path error

--- faithbook/backend/routes/auth.js
+++ faithbook/backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const authenticateToken = require("./middleware/authenticateToken"); // Middleware to check JWT
+const authenticateToken = require("../middleware/authenticateToken"); // Middleware to check JWT
 require("dotenv").config();
 
 const router = express.Router();
