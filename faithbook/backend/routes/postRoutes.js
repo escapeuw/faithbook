0bb62a207ec730f6f8abff4eaf72457bd3cbca41
escapeuw <ghkdehd78999@gmail.com>
2025-03-11 23:41:03 -0400

    debug

--- faithbook/backend/routes/postRoutes.js
+++ faithbook/backend/routes/postRoutes.js
@@ -46,8 +46,10 @@ router.get("/", async (req, res) => {
         });
 
         const postsWithLikes = await Promise.all(posts.map(async (post) => {
-            // Log post id to verify the post exists
-            
+            console.log(Like.associations);
+            console.log(User.associations);
+            console.log(Post.associations);
+
 
             // Find the likes for each post (limit to 2 users as per your original request)
             const likes = await Like.findAll({
